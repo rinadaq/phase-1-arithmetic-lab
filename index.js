df52bd0e440a9629f95c4fcdab561dd860620b67
Rinad Abu Qauod <rinadaq98@gmail.com>
2023-11-28 14:00:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+let num1 = 31;
+let num2=2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+
+let num3 = 25;
+let num4=7;
+let mod = num3 % num4;
+
+let max = Math.max(1, 5, 9, 10, 20);
